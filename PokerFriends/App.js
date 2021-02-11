c850c1836e413119b8236fde1887c3033609feee
David <andraded95@yahoo.com>
2021-02-10 21:22:35 -0800

    npm install
    was able to download react native navigator

--- PokerFriends/App.js
+++ PokerFriends/App.js
@@ -1,7 +1,10 @@
+import 'react-native-gesture-handler';
 import React, { Component, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from '../PokerFriends/Components/Login';
 import Register from '../PokerFriends/Components/Register';
+import{NavigationContainer} from '@react-navigation/native';
+
 
 export default class App extends Component {
   render(){
