54b1e8100dd2d5854684b981363d7bd95f5a6f7f
csmer <csmer@users.noreply.github.com>
2021-02-14 11:45:58 -0800

    Fixed Register Screen

--- PokerFriends/Components/LandingPage.jsx
+++ PokerFriends/Components/LandingPage.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, KeyboardAvoidingView } from 'react-native';
 import HelpButton from './HelpButton'
+import Login from './Login'
+import Register from './Register'
 
 
 export default class LandingPage extends Component {
-    render(){    
-        return (
-        <View style={styles.container}>
-            <HelpButton/>
-        </View>
-        );
-    }
+   render(){    
+      return (
+      <View style={styles.container}>
+          <Register/>
+      </View>
+      );
+  }
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 50,
+    flex: 1,
     backgroundColor: '#2ecc71',
     alignItems: 'center',
     justifyContent: 'center',
