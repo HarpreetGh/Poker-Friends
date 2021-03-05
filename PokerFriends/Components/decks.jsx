81004cb59dc2cb6075c5dbc79cf4db862a98ae1d
David <andraded95@yahoo.com>
2021-03-04 16:57:59 -0800

    card fix_2

--- PokerFriends/Components/decks.jsx
+++ PokerFriends/Components/decks.jsx
@@ -45,8 +45,8 @@ function freshDeck() {
         CARDIMG[i] = '../assets/deckOfCards/PNG/' + (i+1) + '.png';
      }
 
-        return SUITS.flatMap(suit => {
-            return VALUE.map(value => {
+        return VALUE.flatMap(value => {
+            return SUITS.map(suit => {
                 return new Cards(suit, value)
             });
             
