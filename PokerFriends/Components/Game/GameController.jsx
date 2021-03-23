3a264b259ea1187c160206905a70443f3c000e1b
HarpreetGh <70241088+HarpreetGh@users.noreply.github.com>
2021-03-23 13:13:42 -0700

    updateGame, leaveGame, endGame functions up but not tested.
    This commit is just to protect the progress so far, while i try and do some experiments.

--- PokerFriends/Components/Game/GameController.jsx
+++ PokerFriends/Components/Game/GameController.jsx
@@ -79,6 +79,7 @@ export default class GameSetting extends Component {
     var updates = {};
     updates['/games/'+ this.state.matchType + '/' + this.state.fullMatchName + '/player_cards'] = playerCards;
     updates['/games/'+ this.state.matchType + '/' + this.state.fullMatchName + '/deck'] = this.state.deck;
+    updates['/games/'+ this.state.matchType + '/' + this.state.fullMatchName + '/turn'] = 1;
     
     //firebase.database().ref().update(updates);
   }
@@ -87,12 +88,18 @@ export default class GameSetting extends Component {
     var playerNum = this.state.game.players.indexOf(this.state.user.username)
     if(playerNum == 0){
       this.setState({host: true})
-      this.shuffleCards()
-      this.giveOutCards()
-      var myCardHold = this.state.myCards[playerNum]
-      //^ We do this here to avoid the auto fetch feature of firebase
-      this.setupCards()
-      this.setState({myCards: myCardHold})
+      
+      if(this.state.game.turn == 0){
+        this.shuffleCards()
+        this.giveOutCards()
+        var myCardHold = this.state.myCards[playerNum]
+        //^ We do this here to avoid the auto fetch feature of firebase
+        this.setupCards()
+        this.setState({myCards: myCardHold})
+      }
+      else{
+        this.setState({myCards: this.state.game.player_cards[playerNum].myCards})
+      }
       //console.log("host is true");
     }
     else{
@@ -102,6 +109,7 @@ export default class GameSetting extends Component {
 
       //console.log("host is false")
     }
+    
     this.setState({playerNum: playerNum})
     this.setState({ready: true})
   }
@@ -123,8 +131,110 @@ export default class GameSetting extends Component {
       deck.push(gameDeck.cards.shift())
     }
     this.setState({deck: deck})
-  }     
+  }
+  
+  updateGame(newGameData){
+    var currGame = this.state.game
+
+    var updates = {};
+    var matchLocation = '/games/'+ this.state.matchType + '/' + this.state.fullMatchName + '/'
+    const keys = object.keys(currGame)
+
+    for(var i = 0; i < keys.length; i++){
+      if(currGame[keys[i]] != newGameData[keys[i]]){
+        updates[matchLocation + keys[i]] = newGameData[keys[i]];
+      }
+    }
+    firebase.database().ref().update(updates);
+  }
     
+  leaveGame(){ //When player wants leave game in progress
+    var editGame = this.state.game
+    const playernum = this.state.playerNum
+    
+    const quitBalance = editGame.balance[playernum]
+    const chipsRank = quitBalance - editGame.buyIn
+
+    editGame.balance.splice(playernum,1)
+    editGame.move.splice(playernum,1)
+    editGame.player_cards.splice(playernum,1)
+    editGame.players.splice(playernum,1)
+    editGame.ready.splice(playernum,1)
+    editGame.size -= 1
+
+    var updates = {}; 
+    var matchLocation = '/games/'+ this.state.matchType + '/' + this.state.fullMatchName
+
+    if(editGame.size == 0){ //delete game
+      //by setting the data of these location to NULL, the branch is deleted.
+      //https://firebase.google.com/docs/database/web/read-and-write#delete_data
+
+      updates[matchLocation] = null
+      if(this.state.matchType == 'public'){
+        updates['/games/list/' + this.state.fullMatchName] = null
+      }
+    }
+    else{ //update game
+      updates[matchLocation + '/balance']       = editGame.balance
+      updates[matchLocation + '/move']          = editGame.move
+      updates[matchLocation + '/player_cards']  = editGame.player_cards
+      updates[matchLocation + '/players']       = editGame.players
+      updates[matchLocation + '/ready']         = editGame.ready
+      updates[matchLocation + '/size']          = editGame.size
+    }
+    
+    var user = firebase.auth().currentUser;
+    updates['/users/'+ user.uid +'/in_game'] = '';
+    updates['/users/'+ user.uid +'/chips'] = this.state.user.chips + quitBalance;
+    updates['/users/'+ user.uid +'/games'] = this.state.user.games + 1;
+
+    ///change this later to be round based, instead of game based
+    if(chipsRank > 0){
+      updates['/users/'+ user.uid +'/chips_won'] = this.state.user.chips + chipsRank;
+    }
+    else if(chipsRank < 0){
+      updates['/users/'+ user.uid +'/chips_lost'] = this.state.user.chips + chipsRank;
+    }
+
+    firebase.database().ref().update(updates);
+  }
+
+  endGame(){ //When game ends and there is a winner 
+    //maybe insert a if(size > 1) so doesn't count for solos.
+    var endGame = this.state.game
+    const playernum = this.state.playerNum
+    
+    const endBalance = endGame.balance[playernum]
+    const chipsRank = endBalance - endGame.buyIn
+
+    var updates = {}; 
+    var matchLocation = '/games/'+ this.state.matchType + '/' + this.state.fullMatchName
+
+    updates[matchLocation] = null
+    if(this.state.matchType == 'public'){
+      updates['/games/list/' + this.state.fullMatchName] = null
+    }
+    
+    var user = firebase.auth().currentUser;
+    updates['/users/'+ user.uid +'/in_game'] = '';
+    updates['/users/'+ user.uid +'/chips'] = this.state.user.chips + endBalance;
+    updates['/users/'+ user.uid +'/games'] = this.state.user.games + 1;
+
+    if(endGame.balance[playernum] > 0){
+      updates['/users/'+ user.uid +'/wins'] = this.state.user.wins + 1;
+    }
+    
+    ///change this later to be round based, instead of game based
+    if(chipsRank > 0){
+      updates['/users/'+ user.uid +'/chips_won'] = this.state.user.chips + chipsRank;
+    }
+    else if(chipsRank < 0){
+      updates['/users/'+ user.uid +'/chips_lost'] = this.state.user.chips + chipsRank;
+    }
+
+    firebase.database().ref().update(updates);
+  }
+
   render() { 
     if(this.state.ready){
       return(
